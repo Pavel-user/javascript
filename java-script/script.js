27d2fe5aad44582ad43ff9e14c2445de57698b21
Pavel <gorunov80@mail.ru>
2020-05-24 19:42:45 +0300

    second test

--- java-script/script.js
+++ java-script/script.js
@@ -1,6 +1,17 @@
-'use strict';
+" use strict ";
 
-let numberOfFilms = prompt("Сколько фильмов, вы уже посмотрели?", "");
+let numberOfFilms;
+function huwMenyFilms () {
+   numberOfFilms=+prompt("Сколько фильмов, вы уже посмотрели?", "");
+   while(numberOfFilms==null || numberOfFilms==" " || isNaN(numberOfFilms) ) {
+      numberOfFilms=+prompt("Сколько фильмов, вы уже посмотрели?", "");
+
+
+   }
+}
+huwMenyFilms ();
+
+
 let personalMovieDB = {
     count: numberOfFilms,
     movie: {},
@@ -35,29 +46,71 @@ console.log(personalMovieDB);*/
 
 
 // Код возьмите из предыдущего домашнего задания
-for( let i=0; i<2; i++) {
-        const a = prompt('Один из просмотренных фильмов?',''),
-           b = prompt('На сколько оцените его?', '');
-        if( a != null && a != "" && b != null && b != "" && a.length < 50){
-            console.log("done");
-            personalMovieDB.movie[a] =b;
-        } else { console.log("error");
-            i--;
+function oneShowedFilm () {
+   for( let i=0; i<2; i++) {
+      const a = prompt('Один из просмотренных фильмов?',''),
+         b = prompt('На сколько оцените его?', '');
+      if( a != null && a != "" && b != null && b != "" && a.length < 50){
+          console.log("done");
+          personalMovieDB.movie[a] =b;
+      } else { console.log("error");
+          i--;
 
-        }
+      }
 }
- if(personalMovieDB.count < 10){
-     console.log("Просмотрено довольно мало фильмов");
- } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("Вы классический зритель");
 
- } else if(personalMovieDB.count >= 30){
-    console.log("Вы киноман");
+}
+oneShowedFilm();
 
- } else {
-    console.log("Произошла ошибка");
-
- }
+function personalLivel () {
+   if(personalMovieDB.count < 10){
+      console.log("Просмотрено довольно мало фильмов");
+  } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+     console.log("Вы классический зритель");
  
- console.log(personalMovieDB);
+  } else if(personalMovieDB.count >= 30){
+     console.log("Вы киноман");
  
+  } else {
+     console.log("Произошла ошибка");
+ 
+  }
+
+}
+personalLivel();
+
+ 
+ 
+ 
+ /* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+
+// Код возьмите из предыдущего домашнего задания
+function showMyDB(hidden) {
+  
+   if(!hidden) {
+      console.log(personalMovieDB);
+   } 
+}
+showMyDB(personalMovieDB.privat);
+ 
+function writeYourGenres(){
+   for( let i = 1; i<4; i++) {
+      let qwetion = prompt(`Ваш любимый жанр под номером ${i}`, '');
+      personalMovieDB.genres[ i-1] = qwetion;
+   }
+
+}
+writeYourGenres();
+
